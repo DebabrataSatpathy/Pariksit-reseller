9e4f926dcd58832105a3db35258927350add7228
SoumyaDarshanPadhy <soumyadarshanpadhy@gmail.com>
2020-11-20 17:56:46 +0530

    Changed mail..

--- php/mail.php
+++ php/mail.php
@@ -9,7 +9,7 @@ if (isset($_POST['submit'])) {
 	$msg = $_POST['message'];
 	$subject = "Query Regarding Pariksit Reseller";
 	$sendMessage = "You have received a query as \n".$msg."\nFrom ".$name."\nWhose contact number is ".$phone." and Email is ".$mail.".";
-	$header = "From: yogesh@pariksit.com";
+	$header = "From: resellerquery@pariksit.com";
 	if (mail($to, $subject, $sendMessage,$header)) {
 		echo "<script>alert('Mail Sent Success');</script>";
 		
@@ -18,5 +18,4 @@ if (isset($_POST['submit'])) {
 
 	}
 }
-
 ?>
